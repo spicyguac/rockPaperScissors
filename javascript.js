a249f8e59c47eab8143ecab61cfbdd2ce2cec538
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-08-28 23:42:16 -0500

    Add score keeping to the game
    Add +1 score to player in winning scenarios
    Add +1 score to computer in losing scenarios

--- javascript.js
+++ javascript.js
@@ -26,16 +26,22 @@ function playRound(humanChoice, computerChoice) {
         console.log('tie');
     }else if ((humanChoice == 'scissors') && (computerChoice == 'paper')) {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        playerScore += 1;
     }else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        playerScore += 1;
     }else if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        playerScore += 1;
     }else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
+        computerScore += 1;
     }else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
+        computerScore += 1;
     }else if (humanChoice == 'rock' && computerChoice == 'paper') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
+        computerScore += 1;
     }else{
         console.log('Error');
     }
