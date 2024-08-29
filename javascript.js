3ad2609faeafed775f59d06630d434555482627b
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-08-28 20:57:17 -0500

    Create playRound function & Add winning conditions
    Create playRound function to play a round of of
    the game.
    
    Add all conditions where you win the game and the
    condition of when you tie

--- javascript.js
+++ javascript.js
@@ -16,13 +16,24 @@ let computerScore = 0;
 
 function getHumanChoice() {
     let choice = prompt('Rock, Paper, or Scissors?');
-    return choice;f
+    return choice;
 }
 
-
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log('tie');
+    }else if ((humanChoice == 'scissors') && (computerChoice == 'paper')) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+}
 
  
- console.log(getComputerChoice());
- console.log(getHumanChoice());
+playRound(getHumanChoice(),getComputerChoice());
 
  
