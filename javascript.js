01fbca8c375af450f18583e2632123d7c1540ed1
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-08-29 11:10:12 -0500

    Add playGame function & Score visual
    Create playGame to play five rounds of the game
    
    Showed how many points player and computer have

--- javascript.js
+++ javascript.js
@@ -25,30 +25,42 @@ function playRound(humanChoice, computerChoice) {
     console.log(computerChoice);
     if (humanChoice === computerChoice) {
         console.log('tie');
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if ((humanChoice == 'scissors') && (computerChoice == 'paper')) {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         playerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         playerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         playerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
         computerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
         computerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else if (humanChoice == 'rock' && computerChoice == 'paper') {
         console.log(`You lost. ${humanChoice} doesn't beat ${computerChoice}`);
         computerScore += 1;
+        console.log(`Score: \nHuman: ${playerScore}\nComputer ${computerScore}`);
     }else{
         console.log('Error');
     }
 }
 
+function playGame() {
+    for (i = 0; i < 5; i++) { 
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+}
  
-playRound(getHumanChoice(),getComputerChoice());
+playGame();
 
  
