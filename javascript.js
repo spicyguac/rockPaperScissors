24be89882a95313e5bac8b02ef06825972a05cd3
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2024-08-28 20:01:03 -0500

    Add conditionals & adjust generator parameters
    Add conditionals to return computer choice of
    either rock, paper, or scissors
    
    Increased the range of the numbers generated from
    two to three

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
  function getComputerNumber() {
-    let currentRandomNumber = Math.floor(Math.random() * 2 + 1)
-     
+    let currentRandomNumber = Math.floor(Math.random() * 3 + 1)
+    console.log(currentRandomNumber);
+    if (currentRandomNumber == 1){
+        return 'rock';
+    }else if (currentRandomNumber == 2){
+        return 'paper';
+    }else{
+        return 'scissors';
+    }
+
  }
 
  
+ console.log(getComputerNumber());
  
